04ff16a8acf585a8d73c90960dc1b658970a18e9
Rob Nestora <30611018+robnestora@users.noreply.github.com>
2018-10-17 00:53:16 -0400

    tighten up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,13 +8,13 @@ var trivia = {
   clickSound: new Audio("assets/sounds/button-click.mp3"),
   gameHTML: "",
   questionsArray: [
-                  "In what year did the first IBJJF World Jiu-Jitsu Championships take place?", "What is the Abu Dhabi Combat Championship?", "In which country did Brazilian Jiu-Jitsu originate?", "Mata Leon refers to:", "In an IBJJF tournment, passing the guard gets you:"],
+                  "Who cut off Theon Greyjoy's junk?", "Who was Daenarys Targaryen's father?", "Where is the ancestral Stark family home?", "Robb Stark was also known as...", "Tyrion Lannister is good at"],
   answerArray: [
-                ["1991", "1993", "1996", "1998"], ["A MMA event", "An annual submission grappling event", "A biennial submission grappling event", "A BJJ event hosted only in Abu Dhabi"], ["Canada", "Switzerland", "Thailand", "Japan"], ["A foot tickle", "Rear Naked Choke", "There is a lion on the mat", "Wearing shoes on the mat"], ["2 Points", "3 Points", "An instant win", "Dinner after the fight"],],
+                ["Robb Stark", "John Snow", "Ramsay Bolton", "Cersei Lannister"], ["Ned Stark", "Tywin Lannister", "The Mad King", "Bronn"], ["Pyke", "King's Landing", "Moat Cailin", "Winterfell"], ["The Mad King", "The King in the North", "The Bloody Bastard", "The Imp"], ["Chess", "Drinking and knowing things", "Riding horses", "Taming dragons"],],
   correctAnswers: [
-                  "C. 1996", "C. A biennial submission grappling event", "D. Japan", "B. Rear Naked Choke", "B. 3 Points"],
+                  "C. Ramsay Bolton", "C. The Mad King", "D. Winterfell", "B. The King in the North", "B. Drinking and knowing things"],
   imageArray: [
-              "<img class='center-block img-right' src='assets/images/ibjjf-1996.png'>", "<img class='center-block img-right' src='assets/images/adcc.png'>", "<img class='center-block img-right' src='assets/images/japan.jpg'>", "<img class='center-block img-right' src='assets/images/mata-leon.jpg'>", "<img class='center-block img-right' src='assets/images/passing-guard.jpeg'>"],
+              "<img class='center-block img-right' src='assets/images/RamsayBolton.jpg'>", "<img class='center-block img-right' src='assets/images/madking.jpg'>", "<img class='center-block img-right' src='assets/images/winterfell.jpg'>", "<img class='center-block img-right' src='assets/images/RobbStark.jpg'>", "<img class='center-block img-right' src='assets/images/GOT.jpg'>"],
   clock: "",
   questionCounter: 0,
   timeCounter: 20,
@@ -25,7 +25,7 @@ var trivia = {
 //===========================================
 function startScreen(){
   //Create the start button
-  trivia.initialScreen = "<p class='text-center main-button'><a class='btn btn-primary btn-lg start-button text-center' href='#'>Luta!</a></p>";
+  trivia.initialScreen = "<p class='text-center main-button'><a class='btn btn-primary btn-lg start-button text-center' href='#'>Begin</a></p>";
   //Add Start button to main-area
   $(".main-area").html(trivia.initialScreen);
 };
